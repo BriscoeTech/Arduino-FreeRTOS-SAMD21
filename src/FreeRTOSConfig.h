364db2dc0d46454e1116cf795261de4586cfde77
BriscoeTech <Na>
2018-04-01 12:09:18 -0700

    Changed the rtos tick rate to be 1khz. This fixed the timer problems and milli() and micro() function calls

--- src/FreeRTOSConfig.h
+++ src/FreeRTOSConfig.h
@@ -88,7 +88,7 @@
 #define configUSE_IDLE_HOOK				1
 #define configUSE_TICK_HOOK				0
 #define configCPU_CLOCK_HZ				( ( unsigned long ) 48000000 )
-#define configTICK_RATE_HZ				( ( TickType_t ) 10000 )
+#define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 9 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 150 )
 #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 12 * 1024 ) )
