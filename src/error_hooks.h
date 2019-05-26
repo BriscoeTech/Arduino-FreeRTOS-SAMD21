81ce20c937b9741e561eb3aecdc81623c042bba4
Drew Folta <drew@folta.net>
2019-05-25 17:59:56 -0700

    fixed examples/Basic_RTOS_Example

--- src/error_hooks.h
+++ src/error_hooks.h
@@ -16,10 +16,14 @@
 	// this was experimentally chosen from a samd21 processor at 32khz
 	// used to set accuracy of nopDelayMS function
 	#define CAL_FACTOR (F_CPU/6000)
-	
+
 	//**************************************************
 	// function prototypes
 	//**************************************************
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 	// called on fatal error (interrupts disabled already)
 	void rtosFatalError(void);
@@ -29,17 +33,20 @@
 
 	// called on empty stack
 	void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName );
-	
-	
+
+
 	// set the error led to use by the rtos
 	void vSetErrorLed(uint8_t pin, uint8_t activeState);
-	
+
 	// blink an error code out the default led when the rtos has crashed
 	void errorBlink(int errorNumber);
-	
+
 	// will delay the processors using nops
 	// this is used when the rtos has crashed and we cannot use more advanced timing
 	void vNopDelayMS(unsigned long millis);
-	
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
